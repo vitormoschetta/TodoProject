86863b659ca73ae25da0ef8e5a6c169f7d34897e
Vitor <vfarias@bionexo.com>
2022-03-27 23:36:54 -0400

    Add Elastic Stack (search and kibana) in Infra k8s

--- infra/k8s/cluster-init.sh
+++ infra/k8s/cluster-init.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-#kind create cluster --name todo-project
+
+echo "************************************* Creating Cluster ************************************"
+kind create cluster --name todo-project
+
+
+
+echo "**************************** Creating Elasticsearch and Kibana ****************************"
+kubectl apply -f elasticsearch/
+
+sleep 5
+
+kubectl apply -f kibana/
+
+
+
+echo "************************************ Creating Application **********************************"
 
 kubectl apply -f db/
+
+sleep 5
 
 kubectl apply -f api/
 
 kubectl apply -f app/
 
 kubectl apply -f external/
-
-sleep 20
-
-kubectl port-forward service/app-service 6002:6002
